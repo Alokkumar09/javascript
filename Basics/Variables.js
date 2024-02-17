067ecb7fe628d559107a4f88f108ab23888bdf52
Alokkumar09 <147134128+Alokkumar09@users.noreply.github.com>
2024-02-17 19:20:45 +0000

    Container config done

---
+++ Basics/Variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+let name="alok"
+var number= '1234' // ab iska use nhi krte hai kyuki js me block scope and functional ki problem hoti hai 
+//example - agar humne ek varible me change kiya to pura ho jayega 
+console.log([accountId,number
+])
+// console.log([ sare variable ke naam likh do cooma ke saath ]) baar baar console log likhna na pade esi liye
