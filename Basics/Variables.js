0cd7e24a61ed72e68d827d3c97e4bd00659f6fae
Alokkumar09 <147134128+Alokkumar09@users.noreply.github.com>
2024-02-18 07:45:53 +0000

    Container config done

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -5,3 +5,10 @@ var number= '1234' // ab iska use nhi krte hai kyuki js me block scope and funct
 console.log([accountId,number
 ])
 // console.log([ sare variable ke naam likh do cooma ke saath ]) baar baar console log likhna na pade esi liye
+// let temper=null // iska output/type ek object hai
+// let number ; //undefined hai mtlb value assign nhi hai
+
+console.log(null>0) 
+console.log(null>=0)
+// === strictly datatypes ko bhi check krta hai
+//ex console.log("2" === 2) output false aayega
